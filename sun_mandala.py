57caaf5f3339898a9e40368c95759327f42f697b
nellysanchez1 <156357320+nellysanchez1@users.noreply.github.com>
2024-02-11 20:07:57 -0800

    sun_mandala.py

--- sun_mandala.py
+++ sun_mandala.py
@@ -1,9 +1,15 @@
-import turtle
+import turtle  #create turtle module
 
-wn = turtle.Screen()
+#General adjustments, background color, t, and speed
+wn = turtle.Screen() #do general adjustments, background color, t, and speed
 wn.bgcolor('black')
 t = turtle.Turtle()
 t.speed(70)
+
+
+#Circle pattern that will work as the center of the sun 
+'''function to draw inner circle
+'''
 
 def draw_circle():
     circle = [80]
@@ -17,11 +23,15 @@ def draw_circle():
                 t.circle(radius)
                 t.right(20)
 
+
+#Draw a pattern that similuates a simpleflower that worked as the outer layer for the sun
+'''second function, flower shape
+'''
+
 def draw_outersun():
     flower = [60,120]
     colors = ["gold","orange"]
     t.pensize(2)
-
 
     for radius in flower:
         for _ in range(9):
@@ -33,6 +43,11 @@ def draw_outersun():
                 t.left(120)
                 t.left(10)  
 
+
+#Pattern that worked as the edge of the sun
+'''Third function, using triangles to the work as rays
+'''
+
 def draw_sunray():
     t.pensize(1)
     for _ in range(36):
@@ -43,10 +58,10 @@ def draw_sunray():
         t.right(10)
 
 
+#Order in which each pattern will be drawn
 draw_circle()
 draw_sunray()
 draw_outersun()
-
 
 
 
